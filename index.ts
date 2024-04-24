b4984730624a0d49b51c0738a17e7b0e3c23d355
shahzad147 <160399281+shahzad147@users.noreply.github.com>
2024-04-24 17:45:12 +0500

    added a console

--- index.ts
+++ index.ts
@@ -3,6 +3,9 @@
 import inquirer from "inquirer";
 
 const randomNumber = Math.floor(Math.random() * 10 + 1);
+
+console.log(Welcome to number guessing game);
+
 const answers = await inquirer.prompt ([
     {
         name: "userGuessedNumber",
